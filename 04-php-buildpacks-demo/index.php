3d0b35203e55422ca6d0ba24c64d981eb06bba37
yorikane <yorikane@yorikane.altostrat.com>
2025-10-22 02:08:28 +0000

    Add json response header

--- 04-php-buildpacks-demo/index.php
+++ 04-php-buildpacks-demo/index.php
@@ -1,2 +1,5 @@
 <?php
-echo json_encode(['message' => 'Hello ' . htmlspecialchars($_GET['name']) . ' from PHP!']);
+header('Content-Type: application/json; charset=utf-8');
+
+$name = $_GET['name'] ?? 'World';
+echo json_encode(['message' => 'Hello ' . htmlspecialchars($name) . ' from PHP!']);
